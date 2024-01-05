2758e4d57adb02c9b26ef3dc5a0e0c3508ebedbb
cjacobme <cjacobme@aol.com>
2024-01-06 00:02:12 +0100

    Relational entities (#2)
    * implemented the entities
    
    * initialize database
    
    * added first default data
    
    * added first default data
    
    * renamed sql file

---
+++ src/main/java/cj/software/hierarchy/movie/relational/entity/Actor.java
@@ -0,0 +1,65 @@
+package cj.software.hierarchy.movie.relational.entity;
+
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Version;
+import javax.validation.constraints.NotBlank;
+import java.io.Serial;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "actor")
+@SequenceGenerator(name = "genActor", sequenceName = "seq_actor", allocationSize = 1)
+@DynamicInsert
+@DynamicUpdate
+public class Actor implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(generator = "genActor")
+    private Long id;
+
+    @Version
+    @Column(name = "jpa_version", nullable = false)
+    private int version;
+
+    @NotBlank
+    @Column(name = "given_name", nullable = false)
+    private String givenName;
+
+    @NotBlank
+    @Column(name = "family_name", nullable = false)
+    private String familyName;
+
+    public String getGivenName() {
+        return givenName;
+    }
+
+    public void setGivenName(String givenName) {
+        this.givenName = givenName;
+    }
+
+    public String getFamilyName() {
+        return familyName;
+    }
+
+    public void setFamilyName(String familyName) {
+        this.familyName = familyName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+}
---
+++ src/main/schema/Postgres.ddl
@@ -0,0 +1,40 @@
+create sequence seq_actor start 1 increment 1;
+create sequence seq_movie start 1 increment 1;
+create sequence seq_role start 1 increment 1;
+
+    create table actor (
+        id int8 not null,
+        family_name varchar(255) not null,
+        given_name varchar(255) not null,
+        jpa_version int4 not null,
+        primary key (id)
+    );
+
+    create table movie (
+        id int8 not null,
+        title varchar(255) not null,
+        jpa_version int4 not null,
+        primary key (id)
+    );
+
+    create table role (
+        id int8 not null,
+        name varchar(255) not null,
+        jpa_version int4 not null,
+        actor_id int8 not null,
+        movie_id int8 not null,
+        primary key (id)
+    );
+
+    alter table movie 
+        add constraint UK_o6ifx5x4vtwfrpu1a42l8u81w unique (title);
+
+    alter table role 
+        add constraint FKhcvuc3dt3u55owiv7hblb5lfe 
+        foreign key (actor_id) 
+        references actor;
+
+    alter table role 
+        add constraint FKowvo6n9ya7131krugjgxb2ua8 
+        foreign key (movie_id) 
+        references movie;
